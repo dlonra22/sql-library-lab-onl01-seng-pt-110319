f5e6a0a4f4039dc73d8af1b5ee88e1c087d8cf38
arnie b <arnold.bwaila@gmail.com>
2020-03-14 16:24:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,8 +17,8 @@ end
 
 def select_series_title_with_most_human_characters
  "SELECT series.title
-  FROM characters
-  INNER JOIN series
+  FROM series
+  INNER JOIN characters
   ON series.id = characters.series_id
   GROUP BY series.title"
 end
