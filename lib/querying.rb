9497369eeb9a33e2e2995a8d50c88b07fd536ded
arnie b <arnold.bwaila@gmail.com>
2020-03-14 16:22:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,8 +22,7 @@ def select_series_title_with_most_human_characters
   ON series.id = characters.series_id
   GROUP BY series.title
   HAVING characters.species = 'human'
-  ORDER BY COUNT(species) DESC
-  LIMIT 1;"
+  ORDER BY COUNT(species) DESC"
 end
 
 def select_character_names_and_number_of_books_they_are_in
