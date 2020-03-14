004d8583a986803453c9a236a97a1bcfdb199cc8
arnie b <arnold.bwaila@gmail.com>
2020-03-14 16:26:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,11 +16,11 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
- "SELECT series.title
-  FROM series
-  INNER JOIN characters
-  ON series.id = characters.series_id
-  GROUP BY series.title"
+ "SELECT Series.title
+  FROM Series
+  INNER JOIN Characters
+  ON Series.id = Characters.series_id
+  GROUP BY Series.title"
 end
 
 def select_character_names_and_number_of_books_they_are_in
